d076bbf24ca5a9e6c6ebcd2afb8a151862e7219c
Guilherme Leroy <gleroydev@gmail.com>
2023-11-15 18:23:17 -0800

    Add UserRepository.java

---
+++ src/main/java/com/morada/todosimple/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.morada.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.morada.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    
+
+}
