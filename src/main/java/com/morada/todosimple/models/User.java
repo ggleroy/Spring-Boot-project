aa8eaf30ce0a213e2635acf38332743f0a3200c6
Guilherme Leroy <gleroydev@gmail.com>
2023-11-16 09:32:49 -0800

    Update User

--- src/main/java/com/morada/todosimple/models/User.java
+++ src/main/java/com/morada/todosimple/models/User.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -58,6 +59,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
