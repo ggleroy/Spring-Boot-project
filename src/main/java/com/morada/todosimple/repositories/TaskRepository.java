d4a9a7b7107287969d8d16ab10dd73f93f5b26bd
Guilherme Leroy <gleroydev@gmail.com>
2023-11-16 07:44:33 -0800

    Update TaskRepository

--- src/main/java/com/morada/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/morada/todosimple/repositories/TaskRepository.java
@@ -1,16 +1,24 @@
 package com.morada.todosimple.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.morada.todosimple.models.User;
+import com.morada.todosimple.models.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
-    //List<Task> findByUser_id(long id);
+    List<Task> findByUser_id(long id);
 
-    @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
-    List<Task> findByUser_Id(@Param("id") Long id);
+    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
 
 }
